2bc7636fd804d70538215a3ab56428dea68a22fa
AhXaing <ahxaing09@gmail.com>
2023-11-26 18:47:55 +0700

    updated code

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
