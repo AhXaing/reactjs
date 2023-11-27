f6f2f993c8231b5f91ef1adad9666b434080a6b2
AhXaing <ahxaing09@gmail.com>
2023-11-27 17:57:48 +0700

    updated code

--- front-end/src/Pages/Home/HomePage.jsx
+++ front-end/src/Pages/Home/HomePage.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Products from "../../components/Product/Products.jsx";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      {Products.map((items, index) => (
+        <div
+          style={{
+            backgroundColor: "#eee",
+            width: 450,
+            padding: 10,
+            margin: 10,
+            display: "flex",
+          }}
+          key={index}
+        >
+          <div style={{}}>
+            <img style={{ width: "20%" }} src={items.image} />
+            <div>Product Name: {items.name}</div>
+            <div>Price: ${items.price}</div>
+            <div>Qty: {items.qty} pcs</div>
+            <div>Total: ${items.price * items.qty}</div>
+          </div>
+          {items.color.map((items1, index) => (
+            <div style={{ display: "flex" }} key={index}>
+              <div
+                style={{
+                  backgroundColor: items1,
+                  width: 42,
+                  height: 42,
+                  margin: 2,
+                  textAlign: "center",
+                  color: "white",
+                  display: "flex",
+                }}
+              >
+                {items1}
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ front-end/src/components/Product/Products.jsx
@@ -0,0 +1,51 @@
+import tiger from "../../assets/images/tiger.png";
+import singha from "../../assets/images/singha.png";
+import sting from "../../assets/images/sting.png";
+import sting1 from "../../assets/images/sting.jpg";
+import Hanuman from "../../assets/images/Hanuman.png";
+import coca from "../../assets/images/coca-cola.png";
+const Products = [
+  {
+    name: "Tiger",
+    price: 3,
+    qty: 24,
+    image: tiger,
+    color: ["red", "blue", "green", "cyan"],
+  },
+  {
+    name: "Coca Cola",
+    price: 2,
+    qty: 24,
+    image: coca,
+    color: ["red", "blue", "green", "cyan"],
+  },
+  {
+    name: "Hanuman",
+    price: 5,
+    qty: 24,
+    image: Hanuman,
+    color: ["red", "blue", "green", "cyan"],
+  },
+  {
+    name: "Sting",
+    price: 6,
+    qty: 24,
+    image: sting,
+    color: ["red", "blue", "green", "cyan"],
+  },
+  {
+    name: "Sting Red",
+    price: 1.5,
+    qty: 24,
+    image: sting1,
+    color: ["red", "blue", "green", "cyan"],
+  },
+  {
+    name: "Singha",
+    price: 10,
+    qty: 24,
+    image: singha,
+    color: ["red", "blue", "green", "cyan"],
+  },
+];
+export default Products;
